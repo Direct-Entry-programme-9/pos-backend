6f62f0c4d2ee92aa964d96eeddb1f3e8dface6cb
pasanweerawardana <pasanm16gmail.com>
2022-11-01 16:23:59 +0530

    1st commit

--- src/main/java/lk/ijse/dep9/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ItemServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class ItemServlet extends HttpServlet2 {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("items doGet()");
+            response.getWriter().println("item doGet()");
     }
 
     @Override
--- src/main/java/lk/ijse/dep9/api/OrderServlet.java
+++ src/main/java/lk/ijse/dep9/api/OrderServlet.java
@@ -1,16 +1,26 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(name = "OrderServlet", value = "/orders/*")
 public class OrderServlet extends HttpServlet {
+    @Resource(lookup = "java:/comp/env/jdbc/dep9-pos")
+    private DataSource pool;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("orders doGet()");
+        try (Connection connection = pool.getConnection()) {
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
