14c4b17e0c15117101fcd2f2b98ca494407d5cb5
Chathura Rupasinghe <chathura.rmc93@gmail.com>
2022-11-01 16:17:40 +0530

    starting

--- src/main/java/lk/ijse/dep9/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ItemServlet.java
@@ -1,14 +1,22 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import lk.ijse.dep9.api.util.HttpServlet2;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
-@WebServlet(name = "ItemServlet", value = "/items/*")
+@WebServlet(name = "ItemServlet", value = "/items/*", loadOnStartup = 0)
+
+
+
+
 public class ItemServlet extends HttpServlet2 {
+    @Resource(lookup = "java:/comp/env/jdbc/dep9-lms")
+    private DataSource pool;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("items doGet()");
@@ -26,6 +34,7 @@ public class ItemServlet extends HttpServlet2 {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("items doDelete()");
+
+        }
     }
 }
