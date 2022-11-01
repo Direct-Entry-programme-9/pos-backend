203a61e7e4bee7b0d36a7d891a5d5d6867a03046
pasanweerawardana <pasanm16gmail.com>
2022-11-01 19:35:22 +0530

    close #13

--- src/main/java/lk/ijse/dep9/api/OrderServlet.java
+++ src/main/java/lk/ijse/dep9/api/OrderServlet.java
@@ -13,7 +13,9 @@ import lk.ijse.dep9.dto.OrderDTO;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.UUID;
 
 @WebServlet(name = "OrderServlet", value = "/orders/*")
 public class OrderServlet extends HttpServlet {
@@ -33,9 +35,28 @@ public class OrderServlet extends HttpServlet {
             }
             Jsonb jsonb = JsonbBuilder.create();
             OrderDTO order = jsonb.fromJson(request.getReader(), OrderDTO.class);
-        }
-        catch (JsonbException e) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
+            try (Connection connection = pool.getConnection()) {
+                order.setId(UUID.randomUUID().toString());
+                PreparedStatement stm = connection.prepareStatement("INSERT INTO order (id, date, customer) VALUES (?, ?, ?, ?)");
+                stm.setString(1, order.getId());
+                stm.setDate(2, order.getDate());
+                stm.setString(3, order.getCustomerId());
+                int affectedRows = stm.executeUpdate();
+                if (affectedRows == 1) {
+                    response.setStatus(HttpServletResponse.SC_CREATED);
+                    response.setContentType("application/json");
+                    Jsonb jsonb1 = JsonbBuilder.create();
+                    jsonb1.toJson(order, response.getWriter());
+                }
+            } catch (JsonbException e) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
         }
     }
 }
